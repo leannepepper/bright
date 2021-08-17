52e0f3552c994df3fdf56ef8b27acd333148b4cf
Leanne Werner <leanne.werner.lw@gmail.com>
2021-08-17 12:12:40 -0700

    wip, fixing mouse trail effect

--- src/script.js
+++ src/script.js
@@ -9,13 +9,14 @@ import vertexShader from './shader/vertex-shader.glsl'
  * Base
  */
 // Debug
-const gui = new dat.GUI()
+//const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0x23395d)
 
 /**
  * Fonts
@@ -40,13 +41,13 @@ const scene = new THREE.Scene()
                 bevelSegments: 15
              }
          )
-         const color = new THREE.Color('#23395d');
+         const color = new THREE.Color('#add8e6');
          textGeometry.center()
          const titleTextGeometry = new THREE.TextGeometry(
             'creative coder',
             {
                font: font,
-               size: 0.2,
+               size: 0.3,
                height: 0.02,
                curveSegments: 12,
                bevelEnabled: true,
@@ -59,7 +60,7 @@ const scene = new THREE.Scene()
         titleTextGeometry.center();
         titleTextGeometry.translate(
            - (0 - 0.002) * 0.5, // Subtract bevel size
-           - (2.5 - 0.002) * 0.5, // Subtract bevel size
+           - (1.5 - 0.002) * 0.5, // Subtract bevel size
            - (0) * 0.5  // Subtract bevel thickness
         )
          const textMaterial = new THREE.MeshBasicMaterial({ color: color })
@@ -74,7 +75,7 @@ const scene = new THREE.Scene()
  * Particles
  */
 const parameters = {}
-parameters.count = 100
+parameters.count = 150
 parameters.size = 5.1
 parameters.radius = 5
 parameters.branches = 3
@@ -89,64 +90,84 @@ parameters.outsideColor = '#5006aa'
 const geometry = new THREE.BufferGeometry()
 
 const positions = new Float32Array(parameters.count * 3)
+const next = new Float32Array(parameters.count * 3)
+const prev = new Float32Array(parameters.count * 3)
 const colors = new Float32Array(parameters.count * 3)
 const scales = new Float32Array(parameters.count)
 
 for (let i = 0; i < parameters.count; i++) {
     const i3 = i * 3
+    const prevPosition = (i - 1) * 3
+    const nextPosition = (i + 1) * 3
 
+  
     const x = (i / (parameters.count - 1) - 0.5) * 3;
     const y = Math.sin(i / 10.5) * 0.5;
-    const z = 1;
-
-
+    
+    const prevX = ((i -1) / (parameters.count - 1) - 0.5) * 3;
+    const prevY = Math.sin((i -1) / 10.5) * 0.5;
+    
+    const nextX = ((i + 1) / (parameters.count - 1) - 0.5) * 3;
+    const nextY = Math.sin((i + 1) / 10.5) * 0.5;
+    
     positions[i3] = x;
     positions[i3 + 1] = y;
     positions[i3 + 2] = 1
 
-//   const radius = Math.random() * parameters.radius
+    next[nextPosition] = prevX;
+    next[nextPosition + 1] = prevY;
+    next[nextPosition + 2] = 1
+
+    prev[prevPosition] = nextX;
+    prev[prevPosition + 1] = nextY;
+    prev[prevPosition + 2] = 1
+
+   const radius = Math.random() * parameters.radius
 
 
-//   const colorInside = new THREE.Color(parameters.insideColor)
-//   const colorOutside = new THREE.Color(parameters.outsideColor)
-//   const mixedColor = colorInside.clone()
-//   mixedColor.lerp(colorOutside, radius / parameters.radius)
+   const colorInside = new THREE.Color(parameters.insideColor)
+   const colorOutside = new THREE.Color(parameters.outsideColor)
+   const mixedColor = colorInside.clone()
+   mixedColor.lerp(colorOutside, radius / parameters.radius)
 
-//   colors[i3] = mixedColor.r
-//   colors[i3 + 1] = mixedColor.g
-//   colors[i3 + 2] = mixedColor.b
+  colors[i3] = mixedColor.r
+  colors[i3 + 1] = mixedColor.g
+  colors[i3 + 2] = mixedColor.b
 
-//   // Scale
-//   scales[i] = 20;//Math.random()
+  // Scale
+  scales[i] = 20;//Math.random()
 }
 
 geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-//geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-//geometry.setAttribute('a_scale', new THREE.BufferAttribute(scales, 1))
+geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+geometry.setAttribute('next', new THREE.BufferAttribute(next, 3))
+geometry.setAttribute('prev', new THREE.BufferAttribute(prev, 3))
+geometry.setAttribute('a_scale', new THREE.BufferAttribute(scales, 1))
 
 /**
  * Material
  */
 const material = new THREE.ShaderMaterial({
   vertexShader: vertexShader,
-  //fragmentShader: fragmentShader,
+  fragmentShader: fragmentShader,
   depthWrite: false,
   blending: THREE.AdditiveBlending,
   vertexColors: true,
   uniforms: {
-    u_size: { value: 10 },
+    u_size: { value: 5 },
     u_time: { value: 0 },
     u_mouse: { value: new THREE.Vector3() },
-    u_resolution: { value: new THREE.Vector2() },
+    u_resolution: { value: new THREE.Vector2() }
   }
 })
+
+
 
 /**
  * Points
  */
 const points = new THREE.Line(geometry, material)
 scene.add(points)
-scene.background = new THREE.Color("#000")//new THREE.Color("#D7EDFF")
 
 
 /**
@@ -231,23 +252,26 @@ const tick = () => {
   // Update material
   material.uniforms.u_time.value = elapsedTime
   
-for (let i = 0; i < parameters.count; i++) {
-    const i3 = i * 3
-    if(i3 === 0){ 
-        positions[0] = material.uniforms.u_mouse.value.x;
-        positions[1] = material.uniforms.u_mouse.value.y;
-        positions[2] = material.uniforms.u_mouse.value.z;
-    } 
-    else {
-        const tempVec3 = new THREE.Vector3( positions[i3],  positions[i3 + 1],  positions[i3 + 2])
-        const tempLerp = material.uniforms.u_mouse.value.lerp(tempVec3, 0.08);
+    for (let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3
+        const prev = (i - 1) * 3
+
+        if(i3 === 0){ 
+            positions[0] = material.uniforms.u_mouse.value.x;
+            positions[1] = material.uniforms.u_mouse.value.y;
+            positions[2] = material.uniforms.u_mouse.value.z;
+        } 
+        else {
+            const tempVec3 = new THREE.Vector3( positions[i3],  positions[i3 + 1],  positions[i3 + 2])
+            const tempPrevVec3 = new THREE.Vector3( positions[prev],  positions[prev + 1],  positions[prev + 2])
+            const tempLerp = tempVec3.lerp(tempPrevVec3, 0.9);
+            
+            positions[i3] = tempLerp.x;
+            positions[i3 + 1] = tempLerp.y;
+            positions[i3 + 2] = tempLerp.z;
+        }
         
-        positions[i3] = tempLerp.x;
-        positions[i3 + 1] = tempLerp.y;
-        positions[i3 + 2] = tempLerp.z;
     }
-    
-}
 geometry.attributes.position.needsUpdate = true;
 
 
--- src/shader/vertex-shader.glsl
+++ src/shader/vertex-shader.glsl
@@ -1,46 +1,14 @@
-//  // Uniforms 
-// uniform float u_size;
-// uniform float u_time;
-// uniform vec3 u_mouse;
-
-// // Attributes
-// attribute float a_scale;
-
- varying float v_time;
- varying vec3 v_color;
-
-//  void main()
-//         {
-//             /**
-//              * Position
-//              */
-//             vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-//             vec4 viewPosition = viewMatrix * modelPosition;
-//             vec4 projectedPosition = projectionMatrix * viewPosition;
-//             //gl_Position = projectedPosition;
-//             //gl_Position = vec4( position, 1.0 );
-//             gl_Position = vec4( position.x , position.y, 1.0, 1.0 );
-
-//             /**
-//              * Size
-//              */
-//             gl_PointSize = u_size * a_scale;
-//             gl_PointSize *= (1.0 / - viewPosition.z);
-
-//             /**
-//              * Assignments
-//              */
-//             v_time = u_time;
-//             v_color = color;
-//         }
-
-//attribute vec3 position;
 attribute vec3 next;
 attribute vec3 prev;
-//attribute vec2 uv;
 attribute float side;
+attribute float a_scale;
 
 uniform vec2 u_resolution;
+uniform float u_size;
+uniform float u_time;
+
+varying float v_time;
+varying vec3 v_color;
 
 vec4 getPosition() {
     vec2 aspect = vec2(u_resolution.x / u_resolution.y, 1);
@@ -50,15 +18,24 @@ vec4 getPosition() {
     vec2 tangent = normalize(nextScreen - prevScreen);
     vec2 normal = vec2(-tangent.y, tangent.x);
     normal /= aspect;
-    normal *= uv.y * 0.2;
+    normal *= 0.1;
     
     vec4 current = vec4(position, 1);
-   // current.xy -= normal * side;
+     current.xy -= normal * 1.0;
     return current;
     
 }
 
 void main() {
     gl_Position = getPosition();
+    vec4 modelPosition = modelMatrix * vec4((position.x),(position.y),(position.z), 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+   // gl_Position = projectedPosition;
+    v_color = color;
+    
+  //  gl_PointSize = u_size * a_scale;
+   // gl_PointSize *= (1.0 / - viewPosition.z);
     //gl_Position = vec4( position, 1.0 );
 }
